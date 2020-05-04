aa99cb2360c662a6d4342f6d2a1202773d40fe33
Kevin Li <kli@cern.ch>
2020-05-04 09:28:07 +0200

    Add frontend

---
+++ frontend/src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero'
+import { Heroes } from '../heroes_list'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: "Caramon"
+  }
+
+  heroes_list = Heroes
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
