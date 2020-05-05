5ac198aaeae21af6c9faa744e197ef5d80614965
Kevin Li <kli@cern.ch>
2020-05-05 12:35:29 +0200

    Improve button styling in app.

--- frontend/src/app/heroes_list.ts
+++ frontend/src/app/heroes_list.ts
@@ -1,14 +1,14 @@
-import { Hero } from './hero'
+import { Hero } from './hero';
 
 export const Heroes: Hero[] = [
-  { id: 1, name: "Caramon" },
-  { id: 2, name: "Raistlin" },
-  { id: 3, name: "Tasslehof" },
-  { id: 4, name: "Tanis" },
-  { id: 5, name: "Riverwind" },
-  { id: 6, name: "Goldmoon" },
-  { id: 7, name: "Hordak" },
-  { id: 8, name: "Skeletor" },
-  { id: 9, name: "King Kong" },
-  { id: 10, name: "Godzilla" },
-]
+  { id: 1, name: 'Caramon' },
+  { id: 2, name: 'Raistlin' },
+  { id: 3, name: 'Tasslehof' },
+  { id: 4, name: 'Tanis' },
+  { id: 5, name: 'Riverwind' },
+  { id: 6, name: 'Goldmoon' },
+  { id: 7, name: 'Hordak' },
+  { id: 8, name: 'Skeletor' },
+  { id: 9, name: 'King Kong' },
+  { id: 10, name: 'Godzilla' },
+];
