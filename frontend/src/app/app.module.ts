c916129be718719b9a6476a2535a3c0ce567c23a
Kevin Li <kli@cern.ch>
2020-05-06 11:24:49 +0200

    Add hourglass application.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HourglassComponent } from './hourglass/hourglass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HourglassComponent
   ],
   imports: [
     FormsModule,
